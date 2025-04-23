3ddf0c85ced08b831e81673f8aeaab864b00d56b
Luis Kaio <ytkkaio16@gmail.com>
2025-04-23 20:43:44 -0300

    bug: resolvendo import

--- src/components/ultimanoticia/ultimasnoticias.jsx
+++ src/components/ultimanoticia/ultimasnoticias.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styles from "./ultimasNoticias.module.css"; // Usaremos este arquivo CSS
+import styles from "./ultimasnoticias.module.css"; // Usaremos este arquivo CSS
 import { useNavigate } from "react-router-dom";
 
 const UltimasNoticias = () => {
